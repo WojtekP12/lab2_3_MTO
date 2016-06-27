359b38a96a8e92c630e134551fa179f8fc28829f
Wojciech Pełka <nero1993@onet.pl>
2016-06-27 12:15:43 +0200

    poprawa :/ ponowne dodanie check :/

--- src/main/java/edu/iis/mto/similarity/Search.java
+++ src/main/java/edu/iis/mto/similarity/Search.java
@@ -9,4 +9,13 @@ public class Search
 	public static SequenceSearcher getSequenceSearcher() {
  		return new SequenceResult();
  	}
+	
+	public static int check(Object object, Method method) {
+ 		try {
+ 			return object.getClass().getDeclaredField(method.getName()).getInt(object);
+ 		} catch (Exception e) {
+ 			e.printStackTrace();
+ 		}
+ 		return -1;
+ 	}
 }
