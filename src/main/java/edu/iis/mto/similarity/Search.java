e3fd310570b55a0007f9503b752bddf8041c43a7
Wojciech Pełka <nero1993@onet.pl>
2016-06-27 12:13:55 +0200

    usuniecie niepotrzebnej metody

--- src/main/java/edu/iis/mto/similarity/Search.java
+++ src/main/java/edu/iis/mto/similarity/Search.java
@@ -9,13 +9,4 @@ public class Search
 	public static SequenceSearcher getSequenceSearcher() {
  		return new SequenceResult();
  	}
-	
-	public static int check(Object object, Method method) {
- 		try {
- 			return object.getClass().getDeclaredField(method.getName()).getInt(object);
- 		} catch (Exception e) {
- 			e.printStackTrace();
- 		}
- 		return -1;
- 	}
 }
