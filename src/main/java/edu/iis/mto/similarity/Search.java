bedbc3d9b91aebb4e2e24555341143ec79ffb720
Wojciech Pełka <nero1993@onet.pl>
2016-06-27 12:09:57 +0200

    dodanie metody checko do klasy Search

--- src/main/java/edu/iis/mto/similarity/Search.java
+++ src/main/java/edu/iis/mto/similarity/Search.java
@@ -1,10 +1,21 @@
 package edu.iis.mto.similarity;
 
+import java.lang.reflect.Method;
+
 import edu.iis.mto.search.SequenceSearcher;
 
 public class Search 
 {
 	public static SequenceSearcher getSequenceSearcher() {
  		return new SequenceResult();
+ 	}
+	
+	public static int check(Object object, Method method) {
+ 		try {
+ 			return object.getClass().getDeclaredField(method.getName()).getInt(object);
+ 		} catch (Exception e) {
+ 			e.printStackTrace();
+ 		}
+ 		return -1;
  	}
 }
